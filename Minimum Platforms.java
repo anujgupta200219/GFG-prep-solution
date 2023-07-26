e99c42db0732497565422b3462d8f952dee8c4bb
Anuj <113493490+anujgupta200219@users.noreply.github.com>
2023-07-26 18:52:32 +0530

    Create Minimum Platforms.java

---
+++ Minimum Platforms.java
@@ -0,0 +1,30 @@
+
+
+//User function Template for Java
+
+class Solution
+{
+    //Function to find the minimum number of platforms required at the
+    //railway station such that no train waits.
+    static int findPlatform(int arr[], int dep[], int n)
+    {
+        Arrays.sort(arr);
+        Arrays.sort(dep);
+        int count=0;
+        int ans=0;
+        int i=0,j=0;
+        while(i<n){
+            if(arr[i]<=dep[j]){
+                count++;
+                ans=Math.max(ans,count);
+                i++;
+            }
+            else{
+                count--;
+                j++;
+            }
+        }
+        return ans;
+    }
+    
+}
